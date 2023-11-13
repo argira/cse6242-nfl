970b3ab59cda5b7e69eec2a6d0f5fdd78aae1583
argira <argira@hotmail.com>
2023-11-12 17:35:28 -0800

    update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 import teamdecisions
-import bestdecisions
-import worstdecisions
-import rulingteam
+
 
 OPTIONS = { "Team Decisions": teamdecisions,
-           "Best Decisions": bestdecisions,
-           "Worst Decisions": worstdecisions,
-           "4th Down Ruling Teams": rulingteam
+           #"Best Decisions": bestdecisions,
+           #"Worst Decisions": worstdecisions,
+           #"4th Down Ruling Teams": rulingteam
           }
 
 st.sidebar.title('Navigation')
